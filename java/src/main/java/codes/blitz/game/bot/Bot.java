06abb1339507f8b842002351481496a03f713880
Xavier Lida <lidaxavier2005@gmail.com>
2026-01-17 10:30:56 -0500

    Bot 1.0

--- java/src/main/java/codes/blitz/game/bot/Bot.java
+++ java/src/main/java/codes/blitz/game/bot/Bot.java
@@ -1,38 +1,43 @@
 package codes.blitz.game.bot;
 
 import codes.blitz.game.generated.*;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class Bot {
-  Random random = new Random();
+    Random random = new Random();
 
-  public Bot() {
-    System.out.println("Initializing your super mega duper bot");
-  }
-
-  /*
-   * Here is where the magic happens, for now the moves are not very good. I bet you can do better ;)
-   */
-  public List<Action> getActions(TeamGameState gameMessage) {
-    List<Action> actions = new ArrayList<>();
-
-    TeamInfo myTeam = gameMessage.world().teamInfos().get(gameMessage.yourTeamId());
-    if (myTeam.spawners().isEmpty()) {
-      actions.add(new SporeCreateSpawnerAction(myTeam.spores().getFirst().id()));
-    } else if (myTeam.spores().isEmpty()) {
-      actions.add(new SpawnerProduceSporeAction(myTeam.spawners().getFirst().id(), 20));
-    } else {
-      actions.add(
-          new SporeMoveToAction(
-              myTeam.spores().getFirst().id(),
-              new Position(
-                  random.nextInt(gameMessage.world().map().width()),
-                  random.nextInt(gameMessage.world().map().height()))));
+    public Bot() {
+        System.out.println("Initializing Xavier's super mega duper bot");
     }
 
-    // You can clearly do better than the random actions above. Have fun!!
-    return actions;
-  }
+    /*
+     * Here is where the magic happens, for now the moves are not very good. I bet you can do better ;)
+     */
+    public List<Action> getActions(TeamGameState gameMessage) {
+        TeamInfo myTeamInfos = gameMessage.world().teamInfos().get(gameMessage.yourTeamId());
+        List<Action> actions = new ArrayList<>();
+
+        TeamInfo myTeam = gameMessage.world().teamInfos().get(gameMessage.yourTeamId());
+        if (myTeamInfos.nextSpawnerCost() <= myTeamInfos.nutrients()) {
+            actions.add(new SporeCreateSpawnerAction(myTeam.spores().getFirst().id()));
+        }
+
+        for (Spore spore : myTeam.spores()) {
+            actions.add(
+                    new SporeMoveToAction(
+                            spore.id(),
+                            new Position(
+                                    random.nextInt(gameMessage.world().map().width()),
+                                    random.nextInt(gameMessage.world().map().height()))));
+        }
+
+        for (Spawner spawner : myTeam.spawners()) {
+            actions.add(new SpawnerProduceSporeAction(spawner.id(), 1));
+        }
+        // You can clearly do better than the random actions above. Have fun!!
+        return actions;
+    }
 }
